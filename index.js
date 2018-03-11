349a3ae67b3b000178f6bf275bbc31f5ac954a03
chengshiwei <985902547@qq.com>
2018-03-11 16:49:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+	<script>
+		function showHideCode(){
+             $("#showdiv").show();
+             $("#more").hide();
+             $("#less").show();
+          };
+    function aa(){
+             $("#showdiv").hide();
+             $("#less").hide();
+			       $("#more").show();
+          };
+	</script>
